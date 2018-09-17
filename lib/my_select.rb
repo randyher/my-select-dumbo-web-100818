f4647582b95032b7ecf14f490ccf3b3befded7a4
Randy Herasme <randyherasme6@gmail.com>
2018-09-17 20:31:09 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,14 @@
-def my_select(collection)
- # your code here!
+def my_select(arr)
+  output=[]
+  i=0 
+
+  while i<arr.length 
+  if (yield arr[i])
+  output.push(arr[i])
+  end
+  i+=1 
+  
 end
+output
+end
+
